73b8f797367cd4bef28ae9a22f041350d3e7e3bf
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-01 18:04:18 +0200

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,26 +5,22 @@ else
     using Test
 end
 
-# write your own tests here
-@test 2 == 2
-
 import Splittings:compute_interpolants, interpolate
 
 function interpolation_test(nx = 32)
     xmin, xmax = 0.0, 2π
     x = collect(range(xmin,stop=xmax,length=nx+1)[1:end-1])
     y = sin.(2π*x)
-    x_old = x_new = zeros(Float64,nx)
-    y_old = y_new = zeros(Float64,nx)
+    x_new = zeros(Float64,nx)
+    y_new = zeros(Float64,nx)
     coeffs = compute_interpolants(nx, y) # compute spline coefficients
     for (i, xi) in enumerate(x)
-        x_new[i] = xi - rand()
+        x_new[i] = xi - 0.1
         x_new[i] = xmin + mod(x_new[i]-xmin,xmax-xmin) # apply periodic boundary conditions
-        x_old[i] = x_new[i]
         y_new[i] = interpolate(coeffs, nx, xmin, xmax, x_new[i])
-        y_old[i] = sin(2π*x_old[i])
     end
-    maximum(abs.(y_old - y_new))
+    maximum(abs.(sin.(2π*x_new) - y_new))
 end
 
+# write your own tests here
 @test interpolation_test() == 0.0
