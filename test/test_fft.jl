d8967ff94271637a30bf9e662a2a776a02ba4fb4
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-06 14:34:19 +0200

    add test_fft

--- test/test_fft.jl
+++ test/test_fft.jl
@@ -1,25 +1,17 @@
 using FFTW, LinearAlgebra
 
-import Splittings:RectMesh1D, compute_rho, compute_e, advection!
+import Splittings:RectMesh1D, compute_rho, compute_e
 
 meshx = RectMesh1D( 0., 2π,  64, endpoint=false)
 meshv = RectMesh1D(-6., 6., 128, endpoint=false)
 
 ϵ, kx = 0.001, 0.5
 
-f = zeros(Complex{Float64},(meshx.nx,meshv.nx))
-fᵀ= zeros(Complex{Float64},(meshv.nx,meshx.nx))
+f = zeros(Float64,(meshx.nx,meshv.nx))
 
 f .= (1.0.+ϵ*cos.(kx*meshx.x))/sqrt(2π) .* transpose(exp.(-0.5*meshv.x.^2))
-transpose!(fᵀ,f)
 
 ρ = compute_rho(meshv, f)
+@test ρ ≈ ϵ*cos.(kx*mesh.x)
 e = compute_e(meshx, ρ)
-
-dt = 0.1
-
-advection!(fᵀ, meshx, meshv, e,  0.5dt)
-transpose!(f,fᵀ)
-e = advection!( f, meshx, meshv, dt)
-transpose!(fᵀ,f)
-advection!(fᵀ, meshx, meshv, e,  0.5dt)
+@test e ≈ ϵ*sin.(kx*mesh.x)/kx
