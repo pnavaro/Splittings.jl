2c2be62110b9f7d26f2cf01995f27e7387784b98
autodocs <autodocs>
2018-10-11 13:04:11 +0000

    build based on 25637ed

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Splittings.jl Documentation",
     "category": "section",
-    "text": "CurrentModule = SplittingsSemi-Lagrangian method"
+    "text": "Operators splitting package to solve equations of the form `fracdUdt = (T+V)U,where T and  V are two differential operators by solving successively the simpler equationsThe abstract type OperatorSplitting implements the composition form of different kinds of composition methods defined by their coefficients.The application of an operator splitting method to a concrete problem is done by extending this type containing on the one hand the data on which the operators act and a specific implementation of the two operators"
+},
+
+{
+    "location": "index.html#Examples-of-applications-are-provided-for-1",
+    "page": "Introduction",
+    "title": "Examples of applications are provided for",
+    "category": "section",
+    "text": "The linear pendulum problem.\nThe Vlasov equation with constant coefficients advection field.\nThe non linear Vlasov-Poisson equations in cartesian coordinates.<b> References </b>E. Hairer, C. Lubich, G. Wanner, Geometrical numerical integration, Springer 2006This code is translated from a Fortran code written by Eric Sonnendrucker (Max-Planck-Institut für Plasmaphysik) and Michel Mehrenberger (Aix-Marseille Université).CurrentModule = SplittingsSemi-Lagrangian method"
 },
 
 {
