62387173990a0eeecf36b506e317b29ebf50cee8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-21 19:46:08 +0200

    Spline periodic periodic OK

--- test/splinepp.jl
+++ test/splinepp.jl
@@ -332,20 +332,39 @@ end
 
 import Splittings: Geometry, meshgrid
 
-geom = Geometry( 101, 101, -5.0, -5.0, 0.1, 0.1 )
+@testset "Spline periodic periodic" begin
 
-println(geom.x1grid)
+    geom = Geometry( 51, 101, -5.0, -5.0, 0.2, 0.1 )
+    x1, x2 = geom.x1grid, geom.x2grid
+    dt = 0.5
+    fc = exp.(-x1.^2) .* transpose(exp.(-x2.^2))
+    spline = SplinePP( geom )
+    interpolate!( spline, fc, dt, dt )
+    fe = exp.(-(x1.-dt).^2) .* transpose(exp.(-(x2.-dt).^2))
+    error = maximum( abs.( fe .- fc ))
+    println( " error  = $error ")
+    @test error < 1e-4
 
-x1, x2 = geom.x1grid, geom.x2grid
+    geom = Geometry( 101, 101, -5.0, -5.0, 0.1, 0.1 )
+    x1, x2 = geom.x1grid, geom.x2grid
+    dt = 0.5
+    fc = exp.(-x1.^2) .* transpose(exp.(-x2.^2))
+    spline = SplinePP( geom )
+    interpolate!( spline, fc, dt, dt )
+    fe = exp.(-(x1.-dt).^2) .* transpose(exp.(-(x2.-dt).^2))
+    error = maximum( abs.( fe .- fc ))
+    println( " error  = $error ")
+    @test fc ≈ fe 
 
-dt = 0.5
+    geom = Geometry( 64, 128, (-2π,2π), (-2π,2π),:perxy )
+    x1, x2 = geom.x1grid, geom.x2grid
+    dt = 0.1
+    fc = exp.(-x1.^2) .* transpose(exp.(-x2.^2))
+    spline = SplinePP( geom )
+    interpolate!( spline, fc, dt, dt )
+    fe = exp.(-(x1.-dt).^2) .* transpose(exp.(-(x2.-dt).^2))
+    error = maximum( abs.( fe .- fc ))
+    println( " error  = $error ")
+    @test error < 1e-4
 
-fc = exp.(- x1.^2) .* transpose(exp.(- x2.^2))
-spline = SplinePP( geom )
-interpolate!( spline, fc, dt, 0.0 )
-
-fe = exp.(- (x1.-dt).^2) .* transpose(exp.(-x2.^2))
-
-println( " error ", maximum( abs.( fe .- fc )))
-
-#@test fc ≈ fe
+end
